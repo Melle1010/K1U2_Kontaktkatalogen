fe6c7d36092caac3cc372eae7ec68c4ae02625a9
Melle1010 <melle1010.m@gmail.com>
2025-10-26 16:28:30 +0100

    Add project files.

---
+++ K1U2 - Kontaktkatalogen/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace K1U2___Kontaktkatalogen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
